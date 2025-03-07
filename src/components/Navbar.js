494cf39a905935552dee86f124a3a88e45c226c1
Aman8593 <amanwadgaonkar8593@gmail.com>
2025-03-07 23:43:57 +0530

    courses-dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,34 @@
 import React from "react";
 import Image from "next/image";
-import "../styles/navbar.scss"
+import "../styles/navbar.scss";
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-cont">
         <div className="logo-cont">
-          <Image className="logo" alt="logo" src="/images/logo-removebg.png" height={100} width={300} />
+          <Image
+            className="logo"
+            alt="logo"
+            src="/images/logo-removebg.png"
+            height={100}
+            width={300}
+          />
         </div>
         <div className="navbar-links-cont">
           <div className="navbar-links">
             <a href="#home">Home</a>
             <a href="#about">About Us</a>
             <a href="#about">Gallery</a>
-            <a href="#about">Courses</a>
+            <div className="dropdown">
+              <a href="#courses" className="dropbtn">
+                Courses
+              </a>
+              <div className="dropdown-content">
+                <a href="#free-demo">Free Demo</a>
+                <a href="#basic-price-action">Basic Price Action</a>
+                <a href="#advance-price-action">Advance Price Action</a>
+              </div>
+            </div>
             <a href="#about">Services</a>
             <a href="#contact">Contact Us</a>
           </div>
