31f4ecb6234b37a2a2837d1ee65e9876441f25f5
Eamin05 <eaminjawed@gmail.com>
2025-03-12 21:49:49 +0530

    Updated with fixed navbar css and main container css

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,13 @@ import Section2Comp from "@/components/Section2";
 
 export default function Home() {
   return (
-    <div>
+    <div className="main-conatiner">
       <section id="home">
         <Section1Comp />
       </section>
       <section id="services">
         <Section2Comp />
       </section>
-      
     </div>
   );
 }
