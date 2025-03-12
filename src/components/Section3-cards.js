c28471c43e6bbf3a842350e2eeb90ec57b0bfc9e
Aman8593 <amanwadgaonkar8593@gmail.com>
2025-03-12 23:24:32 +0530

    section3-card-structure

--- src/components/Section3-cards.js
+++ src/components/Section3-cards.js
@@ -1,7 +1,18 @@
 import React from "react";
-
+import "../styles/Section3-card.scss";
 const Section3Cards = ({ imageUrl, title, description, subText, id }) => {
-  return <div>hii</div>;
+  return (
+  <div className="section3-card" key={id}>
+    <div className="card-image">
+      <img src={imageUrl} alt={title} />
+    </div>
+    <div className="card-content">
+      <h3 className="card-title">{title}</h3>
+      <p className="card-description">{description}</p>
+      <p className="card-subtext">{subText}</p>
+    </div>
+  </div>
+  );
 };
 
 export default Section3Cards;
